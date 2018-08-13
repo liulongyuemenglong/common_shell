af710df4a649f26212490a0a794117f1e353ebdc
liulong <liulong@zhangyue.com>
2018-08-13 14:13:05 +0800

    提交修改;增加新的安裝命令

--- machine.sh
+++ machine.sh
@@ -16,6 +16,11 @@ function str_index() {
 PYENV_HOME=$HOME/.pyenv
 OS=0 #0:ubuntu 1:centos
 INSTALL=apt
+echo "创建data目录"
+sudo mkdir -p /data
+sudo mkdir -p ~/software
+sudo chown -R liulong.liulong /data
+echo "创建data目录结束"
 if [ $OS -eq 1 ]; then
     INSTALL= yum
 fi
@@ -24,8 +29,32 @@ if [ ! -e /usr/bin/git ]
 then
     echo "安装git"
     sudo $INSTALL install git
+    git config --global user.name liulong
+    git config --global user.email liulong@zhangyue.com
 else
     echo "git已安装"
+fi
+if [ ! -e /usr/bin/vim ]
+then
+    echo "安装vim"
+    sudo $INSTALL install vim
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    config_path=~/.vim/bundle
+    if [ -e $config_path -a -d $config_path ]
+    then
+        echo "开始安装vim插件"
+        cd $config_path
+        echo "开始安装vim-python-pep8-indent插件"
+        pep8_indent_path="$config_path/vim-python-pep8-indent"
+        if [ ! -e $pep8_indent_path ]
+        then
+    	   git clone https://github.com/hynek/vim-python-pep8-indent.git
+        else
+	   echo "已安装vim-python-pep8_indent"
+        fi
+    fi
+else
+    echo "vim已安装"
 fi
 
 if [ ! -e /usr/bin/zsh ]
@@ -51,11 +80,16 @@ then
     	sudo yum install openssl openssl-devel openssl-static -y
     	sudo yum install sqlite-devel -y
     	sudo yum install bzip2-devel bzip2-libs -y
+    	sudo yum install libcurl-devel -y
     elif [ $OS -eq 0 ]; then
         sudo apt-get -y install build-essential libssl-dev libevent-dev libjpeg-dev libxml2-dev
         sudo apt-get -y install libxslt-dev
         sudo apt-get -y install libmysqlclient-dev
         sudo apt-get -y install zlib1g-dev
+        sudo apt-get -y install libcurl4-openssl-dev
+        sudo apt-get -y install libmemcached-dev
+        sudo apt-get -y install libsqlite3-dev 
+        sudo apt-get -y install sqlite3
     fi
 	
     echo "下载pyenv"
@@ -110,6 +144,11 @@ else
     echo "export VIRTUALENVWRAPPER_VIRTUALENV=$HOME/.pyenv/versions/2.7.12/bin/virtualenv" >>$HOME/.zshrc
     echo "export WORKON_HOME=/data/server/virtualenvs" >>$HOME/.zshrc
     echo "source  $HOME/.pyenv/versions/2.7.12/bin/virtualenvwrapper.sh"  >>$HOME/.zshrc
-    source $HONE/.zshrc
+    source ~/.zshrc
     echo "完成安装虚拟环境"
 fi
+echo "下载pycharm"
+wget -o  ~/software/pycharm.tar.gz -v https://download.jetbrains.8686c.com/python/pycharm-professional-2018.2.1.tar.gz
+cd ~/software
+tar -zxvf pycharm.tar.gz
+echo "下载pycharm结束"
